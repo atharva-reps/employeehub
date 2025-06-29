6396783519d00ac14c629c54e537cc231acc46e6
atharva-reps <atharvakale3011@gmail.com>
2025-06-29 09:45:43 +0530

    Directory Update

--- employee_management/myenv/Scripts/activate.bat
+++ employee_management/myenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\HP\Downloads\Employee-Management-master\employee_management\myenv
+set VIRTUAL_ENV=C:\Users\HP\OneDrive\Documents\GitHub\employeehub\employee_management\myenv
 
 if not defined PROMPT set PROMPT=$P$G
 
